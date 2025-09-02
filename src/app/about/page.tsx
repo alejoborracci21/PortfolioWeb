e4301fef42ffa51d2f0664b26828e5af2c8a4e84
Alejo Borracci <alejotrabajo2001@hotmail.com>
2025-09-02 15:00:19 -0300

    new cv

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -33,7 +33,7 @@ export default function AboutPage() {
               </p>
               <div className="flex flex-wrap gap-2">
                 <Button variant="outline" className="gap-2" asChild>
-                  <a href="/AlejoB.CV.pdf" download>
+                  <a href="/AlejoB.CV.html" download>
                     <Download className="h-4 w-4" />
                     Descargar CV
                   </a>
